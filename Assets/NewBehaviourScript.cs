aa36201756991a895a7a39c67d15278d31925a33
tunadeveloper <tunahancengizbusiness@gmail.com>
2024-07-20 15:17:48 +0300

    asdasd

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        UpdateAnimation();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+    void UpdateAnimation()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (moveHorizontal != 0 || moveVertical != 0)
+        {
+            animator.SetTrigger("kos");
+        }
+        else
+        {
+            animator.SetTrigger("kosma");
+        }
+    }
+
+    void Move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.MovePosition(transform.position + movement * moveSpeed * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
